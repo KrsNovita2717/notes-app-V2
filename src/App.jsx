44b54207ee88edf59b7c31172da0dbef713edef9
Krs_Novita <novitakristiana77@gmail.com>
2023-11-18 09:34:11 +0700

    Register page

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,57 @@ import ArchivePage from './pages/ArchivePage';
 import AddNewNotePage from './pages/AddNewNotepage';
 import EditPage from './pages/EditPage';
 import NotFoundPage from './pages/NotFoundPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
-  return (
-    <div className='app-container'>
-      <header>
-        <h1>
-          <Link to='/'>My Notes</Link>
-        </h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/archives' element={<ArchivePage />} />
-          <Route path='/notes/new' element={<AddNewNotePage />} />
-          <Route path='/notes/:id' element={<DetailPage />} />
-          <Route path='/notes/edit/:id' element={<EditPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null,
+    };
+  }
+
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className='app-container'>
+          <header>
+            <h1>
+              <Link to='/'>Catatanku</Link>
+            </h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          </main>
+        </div>
+      );
+    }
+
+    return (
+      <div className='app-container'>
+        <header>
+          <h1>
+            <Link to='/'>Catatanku</Link>
+          </h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/archives' element={<ArchivePage />} />
+            <Route path='/notes/new' element={<AddNewNotePage />} />
+            <Route path='/notes/:id' element={<DetailPage />} />
+            <Route path='/notes/edit/:id' element={<EditPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RegisterInput.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RegisterInput extends React.Component {
+  constructor(props) {
+    super(props)
+ 
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      passwordMatchError: '',
+    }
+ 
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onConfirmPasswordChange = this.onConfirmPasswordChange.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+ 
+  onNameChange(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value,
+      };
+    });
+  }
+ 
+  onEmailChange(event) {
+    this.setState(() => {
+      return {
+        email: event.target.value
+      };
+    });
+  }
+ 
+  onPasswordChange(event) {
+    this.setState(() => {
+      return {
+        password: event.target.value
+      };
+    })
+  }
+
+  onConfirmPasswordChange(event) {
+    this.setState(() => {
+      return {
+        confirmPassword: event.target.value
+      };
+    })
+  }
+ 
+  onSubmitHandler(event) {
+    event.preventDefault();
+ 
+    if (this.state.password !== this.state.confirmPassword) {
+      this.setState({ passwordMatchError: 'Password tidak cocok' });
+      return;
+    }
+
+    this.setState({ passwordMatchError: '' });
+    
+    this.props.register({
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+    });
+  }
+ 
+  render() {
+    return (
+      <form onSubmit={this.onSubmitHandler} className='input-register'>
+        <label htmlFor="name">Name</label>
+        <input type="text" value={this.state.name} onChange={this.onNameChange} />
+        <label htmlFor="email">Email</label>
+        <input type="email" value={this.state.email} onChange={this.onEmailChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" autoComplete='current-password' value={this.state.password} onChange={this.onPasswordChange} />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" value={this.state.confirmPassword} onChange={this.onConfirmPasswordChange} />
+        {this.state.passwordMatchError && (
+          <p style={{ color: 'red' }}>{this.state.passwordMatchError}</p>
+        )}
+        <button>Register</button>
+      </form>
+    )
+  }
+}
+ 
+RegisterInput.propTypes = {
+  register: PropTypes.func.isRequired,
+};
+ 
+export default RegisterInput;
